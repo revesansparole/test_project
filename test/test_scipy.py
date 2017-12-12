3f6881c6db2231873b2eb26dca0397c7ffce1417
jchopard <jerome.chopard@itk.fr>
2017-12-12 13:46:45 +0100

    rm scipy test

--- test/test_scipy.py
+++ test/test_scipy.py
@@ -1,12 +1,12 @@
 import numpy as np
-from scipy import ndimage as nd
-
-
-def test_import_is_working():
-    a = np.zeros((5, 5), dtype=int)
-    a[1:4, 1:4] = 1
-    a[2, 2] = 0
-
-    afull = nd.binary_fill_holes(a).astype(int)
-    assert a[2, 2] == 0
-    assert afull[2, 2] == 1
+# from scipy import ndimage as nd
+#
+#
+# def test_import_is_working():
+#     a = np.zeros((5, 5), dtype=int)
+#     a[1:4, 1:4] = 1
+#     a[2, 2] = 0
+#
+#     afull = nd.binary_fill_holes(a).astype(int)
+#     assert a[2, 2] == 0
+#     assert afull[2, 2] == 1
